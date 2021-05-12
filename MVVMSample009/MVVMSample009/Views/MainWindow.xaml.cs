bdaebfc6e0d7ba245f8c09b38bd139f1a51c14fa
emu2021makuake <81235941+emu2021makuake@users.noreply.github.com>
2021-05-13 08:03:13 +0900

    Add files via upload

--- MVVMSample009/MVVMSample009/Views/MainWindow.xaml.cs
+++ MVVMSample009/MVVMSample009/Views/MainWindow.xaml.cs
@@ -3,9 +3,6 @@ using System.Windows;
 
 namespace MVVMSample009.Views
 {
-    /// <summary>
-    /// MainWindow.xaml の相互作用ロジック
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
